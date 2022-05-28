3ce097d6d020c1949289a8f9a2e6bed18e11cbc5
Viashima <72166830+clins10@users.noreply.github.com>
2022-05-28 14:53:32 +0100

    added the input section to collect strings from users

--- main.py
+++ main.py
@@ -12,16 +12,53 @@ COMMENT = "SOLUTION"
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
-    word = word.lower()
-    anagram = anagram.lower()
 
-    if(sorted(word) == sorted(anagram)):
-        print(sorted(word), sorted(anagram))
+    #   COMMENT = 'take user input'
+    # String1 = input('Enter the 1st string :')
+    # String2 = input('Enter the 2nd string :')
+
+    # COMMENT = " check if length matches" 
+    # if len(String1) != len(String2):
+    #if False
+    # print('Strings are not anagram')
+    # else:
+    #sorted function sort string by characters
+    # String1 = sorted(String1)
+    # String2 = sorted(String2)
+    #check if now strings matches
+    # if String1 == String2:
+        #if True
+        # print('Strings are anagram')
+    # else:
+        #  print('Strings are not anagram')
+
+    # COMMENT = "SOLUTION"
+
+    word = word.lower() # make all letters lowercase
+    anagram = anagram.lower() # make all letters lowercase
+
+    # COMMENT = "comparing the length of the two words"
+    # if(sorted(word) == sorted(anagram)):
+        # print(sorted(word), sorted(anagram))
+        # return True
+    # else:
+        # return False
+
+    word = input('Enter the 1st string :') # take user input
+    anagram = input('Enter the 2nd string :') # take user input
+
+    COMMENT = "check if length matches"
+    word = sorted(word)
+    anagram = sorted(anagram)
+    if word == anagram:
         return True
+            # print('Strings are anagram')
     else:
-        return False
-print(find_anagram("snow", "nows"))
-print(find_anagram("below", "elbow"))
+        return False  
+        # print('Strings are not anagram')
+
+print(find_anagram("word", "anagram"))
+
 
 
      
